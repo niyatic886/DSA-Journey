fa911fd93a3208d5b7a5ea3df305c9d0f7cc79e9
niyatic886 <niyatic886@gmail.com>
2026-02-11 17:01:47 +0530

    ABC Right Angle Pattern

---
+++ Patterns/ABCRightAngle.js
@@ -0,0 +1,16 @@
+// ABC
+
+let promt = require('prompt-sync')();
+
+let n = Number(promt("Enter the number: "));
+
+
+
+for(let i = 1; i<=n; i++){
+    let ascii = 65;
+    for(let j =1; j<=i; j++){
+        process.stdout.write(String.fromCharCode(ascii) + " ")
+        ascii++;
+    }
+    console.log();
+}
